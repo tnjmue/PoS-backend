abece8582d939b9277b8d02a8f13e0be7d8d9de3
Tanja Mueller <tmueller19@gmx.de>
2026-02-27 22:59:50 +0100

    adjusted CORS

--- server/config/index.js
+++ server/config/index.js
@@ -2,11 +2,9 @@
 const express = require("express");
 
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
-// https://www.npmjs.com/package/morgan
 const logger = require("morgan");
 
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
-// https://www.npmjs.com/package/cookie-parser
 const cookieParser = require("cookie-parser");
 
 // ℹ️ Needed to accept from requests from 'the outside'. CORS stands for cross origin resource sharing
@@ -14,11 +12,11 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 
-const FRONTEND_URL = process.env.ORIGIN;
+const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 
 // Middleware configuration
 module.exports = (app) => {
-  
+
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like heroku use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
@@ -26,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL]
+      origin: [FRONTEND_URL], credentials: true
     })
   );
 
